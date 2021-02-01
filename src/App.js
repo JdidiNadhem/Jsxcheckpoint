2ee2c06001d9f743135f2d1608091a615a3a9b2e
Jdidi Nadhem <jedidinadhem@gmail.com>
2021-02-01 21:30:26 +0100

    first adding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './style.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+     <div style={{border:"solid 1px black",maxWidth:"100vw"}}/>
+
+ <h1 className="title white">Jdidi Nadhem</h1>
+
+ <br />
+
+ <img src="CVPIC.jpg" />
+
+ <br />
+
+ <img src="/softdev.png" />
+
+ <br />
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/N3AkSS5hXMA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
     </div>
   );
 }
